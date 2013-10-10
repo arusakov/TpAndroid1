7ccd075c517c415bbe6b948ef3c9ae70cb7cd7c6
arusakov <a_s_rusakov@mail.ru>
2013-10-11 01:13:50 +0400

    page2 in horizontal

--- src/technopark/TpAndroidOne/Activity2.java
+++ src/technopark/TpAndroidOne/Activity2.java
@@ -3,13 +3,11 @@ package technopark.TpAndroidOne;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
-	/**
-	 * Called when the activity is first created.
-	 */
+public class Activity2 extends Activity {
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.page3);
+		setContentView(R.layout.page2);
 	}
 }
