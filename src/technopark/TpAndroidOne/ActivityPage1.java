2a46ceaf2a58e20205e191a251cee5ae9505fb21
arusakov <a_s_rusakov@mail.ru>
2013-10-11 19:29:32 +0400

    ok

--- src/technopark/TpAndroidOne/ActivityPage1.java
+++ src/technopark/TpAndroidOne/ActivityPage1.java
@@ -2,12 +2,14 @@ package technopark.TpAndroidOne;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Activity2 extends Activity {
+public class ActivityPage1 extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.page2);
+		setContentView(R.layout.page1);
 	}
 }
---
+++ src/technopark/TpAndroidOne/ActivityPage3.java
@@ -0,0 +1,21 @@
+package technopark.TpAndroidOne;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ActivityPage3 extends Activity {
+
+	//private final DialogPage3 dialogPage3 = new DialogPage3();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+}
